22527845e1aeccd14bc7b96e1bf8d4a56e2d94d1
Roopa Madihally <roopabdr@gmail.com>
2019-04-13 11:02:53 +0530

    Updates

---
+++ precache-manifest.47220f7f5c8b8185358eb8f992a2d920.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4069d56ae62511cdebbf",
+    "url": "/world-clock/static/js/runtime~main.865c0e53.js"
+  },
+  {
+    "revision": "f01d49f01050ed78b357",
+    "url": "/world-clock/static/js/main.8917f916.chunk.js"
+  },
+  {
+    "revision": "6498f9c7812d1aabf805",
+    "url": "/world-clock/static/js/2.85f7d3c4.chunk.js"
+  },
+  {
+    "revision": "f01d49f01050ed78b357",
+    "url": "/world-clock/static/css/main.b09aef46.chunk.css"
+  },
+  {
+    "revision": "a5d06e1b5dd57a7e630431f78e372ddd",
+    "url": "/world-clock/index.html"
+  }
+];
