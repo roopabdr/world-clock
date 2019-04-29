df346fbfd5c54a3ace0a9c0532354e2ab52dda0c
Roopa Madihally <roopabdr@gmail.com>
2019-04-29 23:54:30 +0530

    changed unordered list to casual div elements with span elements

--- src/components/Autosuggestion/Autosuggestion.js
+++ src/components/Autosuggestion/Autosuggestion.js
@@ -57,35 +57,21 @@ class Autosuggestion extends Component {
           <input type='text' 
           className='inputText' 
           placeholder='Enter location name...' 
-          onChange={ (event) => { this.setState({listItem: event.target.value, showSuggestion: true}); return onLocationNameHandler(event.target.value) } }/>
-          {/* <div className='autosuggestion'>
-          <ul id='listing' className='suggestions'>
-            <li>one</li>
-            <li>two</li>
-            <li>three</li>
-            <li>four</li>
-            <li>five</li>
-            <li>six</li>
-            <li>seven</li>
-            <li>eight</li>
-            <li>nine</li>
-            <li>ten</li>
-          </ul>
-          </div> */}
+          onChange={ (event) => { this.setState({listItem: event.target.value, showSuggestion: true}); return onLocationNameHandler(event.target.value) } }/>         
           { 
             showSuggestion === true && listItem !== '' ? 
               <div className='autosuggestion suggestions'>
-              <ul>
+              <div>
               {
                 suggestions.filter((filterItem) => {
                   return filterItem.label.includes(listItem)
                 }).map((mapItem) => {
                   return (
-                    <li key={ mapItem.label }  className={ showSuggestion ? "showlist" : null }>{ mapItem.label }</li>
+                    <span key={ mapItem.label }  className={ showSuggestion ? "showlist" : null }>{ mapItem.label }</span>
                   )
                 })
               }
-              </ul>
+              </div>
               </div>
             : null            
           }   
